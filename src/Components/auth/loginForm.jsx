19a036954bb0505f6a0e5c6a0b5f880372f87ca9
Hijab Zulfiqar <hijabzulfiqar2001@gmail.com>
2024-02-09 12:08:06 +0500

    changes in  login form

--- src/Components/auth/loginForm.jsx
+++ src/Components/auth/loginForm.jsx
@@ -1,36 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { useQuery, keepPreviousData } from '@tanstack/react-query';
+import React from 'react';
+import backdropPath from '../../assets/netflix-background.jpg';
 import { Link } from 'react-router-dom';
 
 const LoginForm = () => {
 
-  const { data: movies } = useQuery({
-    queryKey: ["movies", 40],
-    queryFn: async () => {
-      const response = await fetch(
-        `https://api.themoviedb.org/3/discover/movie?api_key=4d9b181699814fa8a588a90332a200ab&page=40`
-      );
-      const data = await response.json();
-      return data.results;
-    },
-    placeholderData: keepPreviousData,
-  });
 
-  const [currentBackdropIndex, setCurrentBackdropIndex] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentBackdropIndex((prevIndex) => (prevIndex + 1) % (movies ? movies.length : 1));
-    }, 6000);
-
-    return () => clearInterval(interval);
-  }, [movies]);
-
-  const backdropPath = movies ? `https://image.tmdb.org/t/p/original${movies[currentBackdropIndex].backdrop_path}` : null;
-
+  
   return (
     <div className='relative w-full mx-auto h-screen '>
-      {backdropPath && <img className='absolute w-full h-full object-cover mix-blend-overlay' src={backdropPath} alt="Background" />}
+       <img className='absolute w-full h-full object-cover mix-blend-overlay' src={backdropPath} alt="Background" />
       <div className='flex justify-center items-center h-full'>
         <form className='max-w-[400px] text-white font-Abyssinica w-full mx-auto bg-slate-900  p-8'>
           <h2 className='text-4xl font-bold text-center py-4'>Register Here</h2>
